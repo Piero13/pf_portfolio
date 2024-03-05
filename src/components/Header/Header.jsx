3fe73e97185ca991ea3cc48487bf1331a68e0cf4
Pierre Fasce <lemonpiero@gmail.com>
2024-03-05 20:02:55 +0100

    test navlinks

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,51 @@
+import { useState } from 'react';
+
 import logo from '../../assets/logo.png'
+import toggleIcon from '../../assets/toggle_icon.png'
 
 const reload = () => {
     window.location.replace("/#home");
 }
 
 function Header() {
+    const [showNavbar, setShowNavbar] = useState(false);
+    const handleShowNavbar = (item) => {
+        setShowNavbar(!showNavbar);
+    };
+
     return (
         <>
             <header id="header" className="header">
                 <div className="title">
-                    <img src={logo} alt="logo du site" className="title__logo" onClick={reload}/>
+                    <img src={ logo } alt="logo du site" className="title__logo" onClick={ reload }/>
                     <div className="title__text">
                         <h1>Pierre Fasce</h1>
                         <p>Développeur Web</p>
                     </div>
                 </div>
                 <nav className="navbar">
-                    <a href='#home' className='link navbar__link'>Accueil</a>
-                    <a href='#profile' className='link navbar__link'>Profil</a>
-                    <a href='#portfolio' className='link navbar__link'>Portfolio</a>
-                    <a href='#skills' className='link navbar__link'>Compétences</a>
-                    <a href='#footer' className='link navbar__link'>Contact</a>
+                    <div className="ham-menu" onClick={ handleShowNavbar }>
+                        <img src={ toggleIcon } alt="menu format mobile" />
+                    </div>
+                    <div className='navbar__elements'>
+                        <ul>
+                            <li>
+                                <a href='#home' id='homeLink' className='link navbar__link' onClick={ handleShowNavbar }>Accueil</a>
+                            </li>
+                            <li>
+                                <a href='#profile' id='profileLink' className='link navbar__link' onClick={ handleShowNavbar }>Profil</a>
+                            </li>
+                            <li>
+                                <a href='#portfolio' id='portfolioLink' className='link navbar__link' onClick={ handleShowNavbar }>Portfolio</a>
+                            </li>
+                            <li>
+                                <a href='#skills' id='skillsLink' className='link navbar__link' onClick={ handleShowNavbar }>Compétences</a>
+                            </li>
+                            <li>
+                                <a href='#contact' id='contactLink' className='link navbar__link' onClick={ handleShowNavbar }>Contact</a>
+                            </li>
+                        </ul>
+                    </div>
                 </nav>
             </header>
         </>
--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.scss';
 
 import Home from './pages/Home';
+// import Profile from './components/Profile/Profile.jsx';
+// import Portfolio from './components/Portfolio/Portfolio.jsx';
+// import Skills from './components/Skills/Skills.jsx';
+// import Contact from './components/Contact/Contact.jsx';
 import Header from './components/Header/Header.jsx';
 import Footer from './components/Footer/Footer.jsx';
 
